9c03e5ac07038b5b641e7d0122f9728f8c1dbef0
Steinsholt <steinsholt85@hotmail.com>
2012-10-09 17:04:10 +0200

    Renamed a file

---
+++ Traversal/src/uni/agder/traversal/Graph.java
@@ -0,0 +1,17 @@
+package uni.agder.traversal;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Graph {
+	
+	private List<Human> listOfHumans = new ArrayList<Human>();
+	
+	public void generateGraph() {
+			
+			
+	}
+}
+
+
