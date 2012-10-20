0b6e2f751b20cf16c1656c79eab60e3706829481
Haakon Steinsholt <steinsholt85@hotmail.com>
2012-10-20 07:42:31 +0200

    Added some comments and what not

--- Traversal/src/uni/agder/traversal/Graph.java
+++ Traversal/src/uni/agder/traversal/Graph.java
@@ -5,13 +5,23 @@ import java.util.List;
 import java.util.Random;
 
 public class Graph {
-	
+
 	private List<Human> listOfHumans = new ArrayList<Human>();
-	
+	private List<Node> listOfNodes = new ArrayList<Node>();
+
+	/*
+	 * Step one: Create any number of nodes, for instance by using a random
+	 * function with a minimum and maximum (rand.nextInt(max - min + 1) + min).
+	 * Step two: Create branches by adding paths for each node (remember
+	 * that this is a graph and if node A has node B as a path B also
+	 * needs node A as a path).
+	 * Step three: Find all possible paths by traversing the graph
+	 * Step four: Count steps for all possible solutions and print best path
+	 */
 	public void generateGraph() {
-			
-			
-	}
+
+	}			
 }
+
 
 
--- Traversal/src/uni/agder/traversal/Node.java
+++ Traversal/src/uni/agder/traversal/Node.java
@@ -7,14 +7,14 @@ import java.util.Random;
 
 
 public class Node {
-	
+
 	private enum NodeType { STAIRS, BATHROOM, HALLWAY, CAR_DECK }
 	private static final RandomEnum<NodeType> randomEnum =
 			new RandomEnum<NodeType>(NodeType.class);
 	private float chanceOfDeath;
 	private int capacity;
 	private NodeType nodeType;
-	private List<Node> listOfChildren;
+	private List<Node> listOfPaths;
 
 	public int getCapacity() {
 		return capacity;
@@ -32,35 +32,34 @@ public class Node {
 		this.chanceOfDeath = chanceOfDeath;
 	}
 
-	public List<Node> getChildren(){
-		if (this.listOfChildren == null) {return new ArrayList<Node> (); }
-		else return this.listOfChildren;
+	public List<Node> getPath(){
+		if (this.listOfPaths == null) {return new ArrayList<Node> (); }
+		else return this.listOfPaths;
 	}
 
-	public void setChildren(List<Node> listOfChildren){
-		this.listOfChildren = listOfChildren;
+	public void setPath(List<Node> listOfChildren){
+		this.listOfPaths = listOfChildren;
 	}
-
-	public void addChildren(int number){
-		if(this.listOfChildren==null){ new ArrayList<Node> (); }
-		for (int start = 0; start < number; start++){
-			Node node = new Node();
-			this.listOfChildren.add(node);
-		}		
+	// Untested
+	public void addPath(Node node){
+		if(this.listOfPaths==null){ new ArrayList<Node> (); }
+		if(node.listOfPaths==null){ new ArrayList<Node> (); }
+		this.listOfPaths.add(node);
+		node.listOfPaths.add(this);		
 	}	
 
 	public Node(){
 		this.nodeType = randomEnum.random();
 	}
-	
+
 	private static class RandomEnum<E extends Enum<NodeType>> {
 		private static final Random randomGenerator = new Random();
 		private final E[] values;
-		
+
 		public RandomEnum(Class<E> token){
 			values = token.getEnumConstants();
 		}
-		
+
 		public E random() {
 			return values[randomGenerator.nextInt(values.length)];
 		}
