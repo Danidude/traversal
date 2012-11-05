b668d81b2506ced78dbc0fc2f4d2a4d5afeaa84f
Daniel Aasen <daniboy144@gmail.com>
2012-11-05 19:54:00 +0100

    Added a inner class in antSystem

--- Traversal/src/uni/agder/traversal/AntSystem.java
+++ Traversal/src/uni/agder/traversal/AntSystem.java
@@ -1,15 +1,33 @@
 package uni.agder.traversal;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class AntSystem {
 	private int numberOfAnts = 1000;
 	/* Used to calculate the amount of pheromones that evaporate */
 	private double evaporation = 0.25;
 	/* Used to weigh the importance of the pheromone deposits */
 	private double alpha = 0.125;
+	/*Is a map over all the nodes and their pathData*/
+	private Map<Node, AntNodeData> allNodesData;
+	/*List of all the nodes in the graph*/
+	private ArrayList<Node> listOfNodes;
 	Graph graph;
 	
 	public AntSystem(Graph graph){
 		this.graph = graph;
+		listOfNodes = new ArrayList<Node>();
+		listOfNodes = (ArrayList<Node>) graph.getNodes();
+		
+		allNodesData = new HashMap<Node, AntNodeData>();
+		
+		for(Node n: listOfNodes)
+		{
+			AntNodeData nodeData = new AntNodeData(n);
+			allNodesData.put(n, nodeData);
+		}
 	}
 	
 	/*
@@ -31,5 +49,30 @@ public class AntSystem {
 	 * */
 	public void calculateTransitionProbabilities(){
 		
+	}
+	
+	/*
+	 * Contains the proberbility for each path on an node.
+	 */
+	private class AntNodeData
+	{
+		private int nodeID;
+		private Map<Node, Float> proberbiletyOfPath;
+		
+		public AntNodeData(Node n)
+		{
+			nodeID = n.NodeID;
+			proberbiletyOfPath = new HashMap<Node, Float>();
+			inizilizePathProb(n);
+		}
+		
+		private void inizilizePathProb(Node n)
+		{
+			float startProb = 1/n.getPath().size();
+			for(Node n2: n.getPath())
+			{
+				proberbiletyOfPath.put(n2, startProb);
+			}
+		}
 	}
 }
